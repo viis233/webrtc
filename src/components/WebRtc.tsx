472ba70d3cd01597509e7a75dbe5cce116bbfff0
viis233 <u234u235u238@gmail.com>
2022-07-11 18:16:27 +0800

    ice

--- src/components/WebRtc.tsx
+++ src/components/WebRtc.tsx
@@ -16,24 +16,11 @@ class VideoChat {
     this.dataChannelConnected = false;
     this.rtcConfig = {
       iceServers: [
+
         {
-          urls: "stun:openrelay.metered.ca:80",
+          urls: "stun:stun.axialys.net:3478",
         },
-        {
-          urls: "turn:openrelay.metered.ca:80",
-          username: "openrelayproject",
-          credential: "openrelayproject",
-        },
-        {
-          urls: "turn:openrelay.metered.ca:443",
-          username: "openrelayproject",
-          credential: "openrelayproject",
-        },
-        {
-          urls: "turn:openrelay.metered.ca:443?transport=tcp",
-          username: "openrelayproject",
-          credential: "openrelayproject",
-        },
+
       ],
     };
   }
