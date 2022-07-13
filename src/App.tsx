34d69a35ab3923577bd8617f365088684a80ca57
viis233 <u234u235u238@gmail.com>
2022-07-13 14:59:58 +0800

    修改路由

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,18 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Robot, RobotInit, WebRtcConsole} from "./components/WebRtc";
 
 function App() {
-  return (
-      <Router>
-          <Switch>
-              <Route exact path="/console">
-                  <WebRtcConsole />
-              </Route>
-              <Route exact path="/robot">
-                  <RobotInit
-
-                  />
-              </Route>
-          </Switch>
-      </Router>
-  )
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/console">
+                    <WebRtcConsole/>
+                </Route>
+                <Route exact path="/robot">
+                    <RobotInit/>
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
 export default App
